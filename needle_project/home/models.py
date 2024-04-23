553ce585a7aa510ded9f4ebad4c14c587a0eb415
Gaurab Lohani <132120369+gaurablohani01@users.noreply.github.com>
2024-04-23 18:25:18 +0545

    Update models.py

--- needle_project/home/models.py
+++ needle_project/home/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     image = models.ImageField(upload_to='post-pictures', default='defaultimage.png')
-    time = models.DateTimeField(default=timezone.now)  
+    time = models.DateTimeField(default=timezone.now)
     is_deletable = models.BooleanField(default=True)
 
     def delete(self, *args, **kwargs):
@@ -19,3 +19,11 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("detailpost", kwargs={"username": self.author.username, "pk": self.pk})
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey('home.Post', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user} likes {self.post}'
